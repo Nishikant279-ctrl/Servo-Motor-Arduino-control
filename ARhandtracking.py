45bebe524141e8b38391f1aecac464f3dcde343f
Nishikant279-ctrl <nishikantkharat09@gmail.com>
2024-11-22 15:57:52 +0530

    Update ARhandtracking.py

--- ARhandtracking.py
+++ ARhandtracking.py
@@ -1,4 +1,5 @@
 import cv2
+import cvzone.SerialModule
 import mediapipe as mp
 import serial
 import math
@@ -7,7 +8,8 @@ Cam = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
-ser = serial.Serial("COM3", 9600)
+
+# ser = serial.Serial("COM3", 9600)
 
 def calculate_distance(landmark1, landmark2):
   #Extract x and y coordinates
@@ -26,15 +28,27 @@ while True:
         for handlms in release.multi_hand_landmarks:
             for id, lm in enumerate(handlms.landmark):
                 mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
-                if id == 8 and id + 4 == 12:
+                if id == 4 and id + 8 == 12:
                     landmark_8 = handlms.landmark[id]
                     landmark_12 = handlms.landmark[id+4]
 
                     distance = calculate_distance(landmark_8, landmark_12)
-                    print(distance)
+                    # print(distance)
 
-                    data_to_send = distance
-                    ser.write(data_to_send.encode())
+                    # < 0.18
+
+                    # ser.sendData(distance)
+
+                if id == 4 and id + 4 == 8:
+                    landmark_4 = handlms.landmark[id]
+                    landmark_8 = handlms.landmark[id+4]
+
+                    distance2 = calculate_distance(landmark_4, landmark_8)
+
+                    print(distance2)
+
+                    # < 0.08
+                    # > 0.25
 
     cv2.imshow("Hand Gestures", img)
     if cv2.waitKey(1) == 27:

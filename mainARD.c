c827772d11bcb449ce08394f26b9ad2a779bb970
Nishikant279-ctrl <nishikantkharat09@gmail.com>
2024-11-22 21:21:42 +0530

    Create mainARD.c

---
+++ mainARD.c
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  Serial.begin(9600);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop() {
+  if(Serial.available() < 0.18){
+    myservo.write(0);
+    delay(15);
+  }
+  if(Serial.available() < 0.09){
+    myservo.write(75);
+    delay(15);
+  }
+  if(Serial.available() > 0.19){
+    myservo.write(0);
+    delay(15);
+  }
+    
+}

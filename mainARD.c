74776da899b64e208f02c7296e925168bf1a505b
Nishikant279-ctrl <nishikantkharat09@gmail.com>
2024-11-27 14:39:41 +0530

    Update mainARD.cpp

--- mainARD.c
+++ mainARD.c
@@ -1,46 +1,68 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+const int trigPin1 = 2;
+const int echoPin1 = 3;
 
-int pos = 0;    // variable to store the servo position
+const int trigPin2 = 4;
+const int echoPin2 = 5;
+
+const int servoPin1 = 6;
+const int servoPin2 = 7;
+const int servoPin3 = 8;
+const int servoPin4 = 9;
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
 
 void setup() {
-  Serial.begin(9600);
-  myservoH.attach(9); // attaches the servo on pin 9 to the servo object
-  myservoH2.attach(10);
-  myservoM.attach(11);
-  myservoM2.attach(12);
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+
+  servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
+  servo3.attach(servoPin3);
+  servo4.attach(servoPin4);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    float distance = Serial.parseFloat();
+  // Read distance from sensor 1
+  long duration1, distance1;
+  digitalWrite(trigPin1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin1, LOW);
+  duration1 = pulseIn(echoPin1, HIGH);
+  distance1 = duration1 * 0.034 / 2;
 
-    if(distance < 0.18){
-      myservoH.write(0);
-      myservoH2.write(0);
-      delay(15);
-    }
-    else{
-      myservoH.write(180);
-      myservoH2.write(180);
-    }
-    if(distance < 0.09){
-      myservoM.write(75);
-      myservoM2.write(75);
-      delay(15);
-    }
-    if(distance > 0.19){
-      myservoM.write(0);
-      myservoM2.write(0);
-      delay(15);
-    }
-    else{
-      myservoM.write(90);
-      myservoM2.write(0);
-    }
+  // Read distance from sensor 2
+  long duration2, distance2;
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  duration2 = pulseIn(echoPin2, HIGH);
+  distance2 = duration2 * 0.034 / 2;
 
-    delay(50);
-    
+  // Check for hand detection and control servos accordingly
+  if (distance1 < 20) {
+    servo1.write(180);
+  } else {
+    servo1.write(0);
   }
+
+  if (distance2 < 20) {
+    servo2.write(90);
+    servo3.write(0);
+  } else {
+    servo2.write(0);
+    servo3.write(90);
+  }
+
+  delay(100);
+}
